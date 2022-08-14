a899f1ef30243daa56e14a7c44b66592cea51ac4
Repro12 <111174041+Repro12@users.noreply.github.com>
2022-08-14 05:06:28 -0700

    Add files via upload

---
+++ Campus Air Capsule.js
@@ -0,0 +1,11 @@
+function myntra(){
+    window.location.href= "https://www.myntra.com/sports-shoes/campus/campus-men-blue-mesh-air-capsule-running-shoes/16920378/buy";
+};
+
+function ajio(){
+    window.location.href= "https://www.ajio.com/campus-camp-tech-air-capsule-running-shoes/p/460444157_black";
+};
+
+function firstcry(){
+    window.location.href= "https://www.firstcry.com/campus/campus-kidos-air-capsule-detailing-sports-shoes-dark-green-and-parrot-green/11437666/product-detail?ref=GoogleShopping_6_170";
+};
